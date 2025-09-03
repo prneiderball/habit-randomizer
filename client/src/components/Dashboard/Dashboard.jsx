c906a63aabc609e92c51ab48a820f3f48131d887
P.R. Neider-Ball <phillrballn@icloud.com>
2025-09-03 15:51:52 -0500

    Update Dashboard.jsx

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Dashboard.css";
 
 const API_URL = import.meta.env.VITE_API_URL;
+console.log("API_URL:", API_URL);
 
 function Dashboard({ token, user, onLogout }) {
   const [currentUser, setCurrentUser] = useState(
